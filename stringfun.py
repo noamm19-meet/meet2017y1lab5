7c97c5b3f388d502d50a8f61401ad3b27ee1aef6
noamm19-meet <noamm19@meet.mit.edu>
2017-07-30 22:45:51 +0300

    Stopping here

--- stringfun.py
+++ stringfun.py
@@ -3,3 +3,18 @@ def string_test(s):
         return(True)
     else:
         return(False)
+
+
+def add_x(s):
+    #s = input('give me a string: ')
+    a=  'x'+ s +'x'
+    return(a)
+        
+    
+#for i in range(1000):
+#    answer = add_x('hi')
+#    print(answer)
+
+def add_strings(s,a,d):
+    c = s+a+d
+    return c
